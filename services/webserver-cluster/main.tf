d2d3f585b5e248220f45fcb673ce41f13015db36
kang wang <kangqiwang@outlook.com>
2023-11-30 15:48:17 +0000

    update location

--- services/webserver-cluster/main.tf
+++ services/webserver-cluster/main.tf
@@ -150,7 +150,7 @@ data "terraform_remote_state" "db" {
   config = {
     bucket = var.db_remote_state_bucket
     key    = var.db_remote_state_key
-    region = "us-east-2"
+    region = "eu-west-2"
   }
 }
 
